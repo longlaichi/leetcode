08acfc58ec0f233794da6b7ccb67ceff82553f2d
longlaichi <poorveshdewangan87@gmail.com>
2026-08-24 22:39:27 +0530

    Time: 500 ms (8.73%) | Memory: 13.7 MB (78.95%) - LeetSync

---
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>> answer(m, vector<int>(n));
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                int sum = 0;
+                for(int r = i-k;r<=i+k;r++){
+                    for(int c = j-k;c<=j+k;c++){
+                        if(r>=0 && r<m && c>=0 && c<n){\
+                        sum+=mat[r][c];
+                        }
+                    }
+                }
+                answer[i][j] = sum;
+            }
+        }
+        return answer;
+        
+    }
+};
