bb38305e984b34f97f3ade075b3ff01f9db369f8
longlaichi <poorveshdewangan87@gmail.com>
2026-08-30 13:05:53 +0530

    Time: 76 ms (36.40%) | Memory: 165 MB (13.00%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0;
+        int end = 0;
+      for(int i = 0;i<s.size();i++){
+        int len1 = expand(s,i,i);
+        int len2 = expand(s,i,i+1);
+        int len = max(len1,len2);
+        if(len >(end-start)){
+            start = i - (len-1)/2;
+            end = i + len/2;
+        }
+        
+
+      }  
+      return s.substr(start,end-start + 1);
+    }
+    int expand(string s,int left,int right){
+        while(left>=0 && right<s.size() && left<=right){
+            if(s[left]!=s[right]){
+                break;
+
+            }
+            left--; // for expansion
+            right++; // for expansion
+
+        }
+        return right -left - 1;
+    }
+};
